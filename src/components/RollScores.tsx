0b1067c4f84e4ba6be098cd1cedbc9e43e18cce5
pdaulby <p.daulby@hotmail.com>
2023-10-21 16:02:44 +0100

    start the groundwork for method 1 and 2

--- src/components/RollScores.tsx
+++ src/components/RollScores.tsx
@@ -6,25 +6,33 @@ import Store from "../store/Store";
 import { observer } from "mobx-react";
 import { createViableScore } from "../scripts/CalculateScores";
 import { sumXDY, xD6Top3 } from "../scripts/MathUtil";
+import store from "../store/Store";
+import { RaceClassRestrictions, RacialScoreMax, RacialScoreMin } from "../models/Race";
+import { possibleClasses } from "../scripts/MinimumScores";
 
 export const RollScores: React.FC = observer(() => {
     switch (Store.method) {
       case -1:
+        throw Error("should never happen")
       case 0:
         let score0 = createViableScore(Store.race!)
         return <ChoosableStatBlock {...score0} />
       case 1:
+        let rolls1 = Array.from(Array(6)).map(()=>xD6Top3(4)).sort((a, b)=>b-a);
+        return <ArrangeToTaste scores={rolls1} />
       case 2:
-        throw Error("should never happen")
+        let fn = ()=>Math.max(...Array.from(Array(6)).map(()=>sumXDY(3,6)));
+        let rolls2 =  Array.from(Array(6)).map(fn).sort((a, b)=>b-a);
+        return <ArrangeToTaste scores={rolls2} />
       case 3:
-        let score3 = createViableScore(Store.race!, ()=> Math.max(...Array.from(Array(12)).map(()=>sumXDY(3,6))))
+        let score3 = createViableScore(Store.race!, ()=> Math.max(...Array.from(Array(12)).map(()=>sumXDY(3,6))));
         return <ChoosableStatBlock {...score3} />
       case 4:
         let scores: AbilityScores[] = Array.from(Array(12)).map(()=>createViableScore(Store.race!));
         scores.sort(compareScores);
         return (<TwelveGuys scores={scores}></TwelveGuys>);
       case 5:
-        return <>TODO manual input</>
+        throw Error("should never happen")
     }
   })
   
@@ -32,3 +40,15 @@ const TwelveGuys: React.FC<{scores: AbilityScores[]}> =
  ({scores}) => <>{scores.map((s, i) => (<ChoosableStatBlock key={'stats'+i} {...s} />))}</>
 
 export default RollScores;
+
+
+const ArrangeToTaste: React.FC<{scores: number[]}> = observer(({scores}) => {
+    let raceClasses = RaceClassRestrictions.get(store.race!)!;
+    let min = RacialScoreMin.get(store.race!)!;
+    let max = RacialScoreMax.get(store.race!)!;
+    let classes = possibleClasses(scores, raceClasses)
+    return <>
+    <div>{scores}</div>
+    <div>{classes}</div>
+ </>
+});
--- src/scripts/MinimumScores.ts
+++ src/scripts/MinimumScores.ts
@@ -129,3 +129,26 @@ function allowedClass(actualScores: AbilityScores, requiredScores: AbilityScores
     && actualScores.Wisdom >= requiredScores.Wisdom
     && actualScores.Charisma >= requiredScores.Charisma;
 }
+
+export function possibleClasses(scores: number[], allowedClasses: ClassName[] = []): ClassName[] {
+    return Array.from(MinumumScores.entries())
+        .filter(e=>allowedClasses.includes(e[0]))
+        .map(e=>toList(e))
+        .filter(e=>possibleClass(scores, e[1]))
+        .map(e=>e[0])
+}
+
+function toList([c, a]: [ClassName, AbilityScores]): [ClassName, number[]] {
+    let n = [a.Charisma, a.Constitution, a.Dexterity, a.Intelligence, a.Strength, a.Wisdom];
+    n.sort((a,b)=>b-a);
+    return [c,n];
+}
+
+function possibleClass(actualScores: number[], requiredScores: number[]): boolean {
+    return actualScores[0] >= requiredScores[0] 
+    && actualScores[1] >= requiredScores[1] 
+    && actualScores[2] >= requiredScores[2] 
+    && actualScores[3] >= requiredScores[3] 
+    && actualScores[4] >= requiredScores[4] 
+    && actualScores[5] >= requiredScores[5]; 
+}
